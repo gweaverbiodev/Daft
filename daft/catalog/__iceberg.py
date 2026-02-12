bbeb2ce64af35d4fc391d47d8befde785d14a397
gmweaver <gmweaver.usc@gmail.com>
2026-02-12 15:58:22 -0800

    support pyiceberg v0.11.0

--- daft/catalog/__iceberg.py
+++ daft/catalog/__iceberg.py
@@ -85,12 +85,14 @@ class IcebergCatalog(Catalog):
                 raise NotImplementedError(f"Unsupported partition transform: {pf.transform}")
 
             source_field = iceberg_schema.find_field(pf.field.name)
+            field_id = max(1000, len(iceberg_schema.fields)) + idx
+            name = f"__pyiceberg_partition_field_{pf.field.name}_{field_id}"
             iceberg_partition_fields.append(
                 PyIcebergPartitionField(
                     source_id=source_field.field_id,
-                    field_id=1000 + idx,
+                    field_id=field_id,
                     transform=transform,
-                    name=pf.field.name,
+                    name=name,
                 )
             )
         return PyIcebergPartitionSpec(*iceberg_partition_fields)
--- tests/catalog/test_iceberg.py
+++ tests/catalog/test_iceberg.py
@@ -173,7 +173,7 @@ def test_create_partitioned_table_identity(catalog):
     tbl = c.get_table(f"{n}.tbl")
     partition_spec = tbl._inner.spec()
     field = partition_spec.fields[0]
-    assert field.name == "a"
+    assert field.name == "__pyiceberg_partition_field_a_1000"
     assert field.transform.__class__.__name__.lower().startswith("identity")
     if c.has_table(f"{n}.tbl"):
         c.drop_table(f"{n}.tbl")
@@ -195,7 +195,7 @@ def test_create_partitioned_table_bucket(catalog):
     tbl = c.get_table(f"{n}.tbl")
     partition_spec = tbl._inner.spec()
     field = partition_spec.fields[0]
-    assert field.name == "a"
+    assert field.name == "__pyiceberg_partition_field_a_1000"
     assert field.transform.__class__.__name__.lower().startswith("buckettransform")
     if c.has_table(f"{n}.tbl"):
         c.drop_table(f"{n}.tbl")
@@ -217,7 +217,7 @@ def test_create_partitioned_table_truncate(catalog):
     tbl = c.get_table(f"{n}.tbl")
     partition_spec = tbl._inner.spec()
     field = partition_spec.fields[0]
-    assert field.name == "a"
+    assert field.name == "__pyiceberg_partition_field_a_1000"
     assert field.transform.__class__.__name__.lower().startswith("truncatetransform")
     if c.has_table(f"{n}.tbl"):
         c.drop_table(f"{n}.tbl")
@@ -239,7 +239,7 @@ def test_create_partitioned_table_Year(catalog):
     tbl = c.get_table(f"{n}.tbl")
     partition_spec = tbl._inner.spec()
     field = partition_spec.fields[0]
-    assert field.name == "a"
+    assert field.name == "__pyiceberg_partition_field_a_1000"
     assert field.transform.__class__.__name__.lower().startswith("yeartransform")
     if c.has_table(f"{n}.tbl"):
         c.drop_table(f"{n}.tbl")
@@ -261,7 +261,7 @@ def test_create_partitioned_table_Month(catalog):
     tbl = c.get_table(f"{n}.tbl")
     partition_spec = tbl._inner.spec()
     field = partition_spec.fields[0]
-    assert field.name == "a"
+    assert field.name == "__pyiceberg_partition_field_a_1000"
     assert field.transform.__class__.__name__.lower().startswith("monthtransform")
     if c.has_table(f"{n}.tbl"):
         c.drop_table(f"{n}.tbl")
@@ -283,7 +283,7 @@ def test_create_partitioned_table_Day(catalog):
     tbl = c.get_table(f"{n}.tbl")
     partition_spec = tbl._inner.spec()
     field = partition_spec.fields[0]
-    assert field.name == "a"
+    assert field.name == "__pyiceberg_partition_field_a_1000"
     assert field.transform.__class__.__name__.lower().startswith("daytransform")
     if c.has_table(f"{n}.tbl"):
         c.drop_table(f"{n}.tbl")
@@ -305,7 +305,7 @@ def test_create_partitioned_table_Hour(catalog):
     tbl = c.get_table(f"{n}.tbl")
     partition_spec = tbl._inner.spec()
     field = partition_spec.fields[0]
-    assert field.name == "a"
+    assert field.name == "__pyiceberg_partition_field_a_1000"
     assert field.transform.__class__.__name__.lower().startswith("hourtransform")
     if c.has_table(f"{n}.tbl"):
         c.drop_table(f"{n}.tbl")
